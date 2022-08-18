669a1096310f7bea447b134808205178e08bddf8
Lam <lnhlam99@gmail.com>
2022-08-18 08:08:34 +0700

    fearture-09-fearture

--- tim-gia-tri-trong-mang.js
+++ tim-gia-tri-trong-mang.js
@@ -3,5 +3,6 @@ let numbers = [-3, 5, 1, 3, 2, 10];
 for (let i = 0; i < numbers.length;i++) {
     if (value == numbers[i]) {
         alert("value" + numbers[i] + "found at" + i);
-    }
+    } else
+        alert("not found at");
 }
